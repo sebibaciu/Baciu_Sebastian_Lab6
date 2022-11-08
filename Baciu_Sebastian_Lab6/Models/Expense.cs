db0683cdcd4cda506fc6f580df10dc64ef0170d7
bacik <bacik@DESKTOP-QQSVM0D>
2022-11-08 18:28:10 +0200

    Add project files.

---
+++ Baciu_Sebastian_Lab6/Models/Expense.cs
@@ -0,0 +1,11 @@
+﻿namespace Baciu_Sebastian_Lab6.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Secret { get; set;  }
+    }
+}
---
+++ Baciu_Sebastian_Lab6/Models/ExpenseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Baciu_Sebastian_Lab6.Models
+{
+    public class ExpenseDTO
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
